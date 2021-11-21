0abe3f3b5ba985d693bac61e1474f65b81e0f734
William Park <l.jimenez.leon97@gmail.com>
2021-11-20 20:21:51 -0600

    add redes sociales

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -10,22 +10,22 @@ export const Header = () => {
                 <div className="header__menuRedes--contenedor">
                     <ul className="header__menuRedes">
                         <li>
-                            <a rel="noreferrer" href="https://www.facebook.com" target="_blank">
+                            <a rel="noreferrer" href="https://www.facebook.com/luisenrique.kurosaki.5/" target="_blank">
                                 <i className="fab fa-facebook-square"></i>
                             </a>
                         </li>
                         <li>
-                            <a rel="noreferrer" href="https://www.instagram.com" target="_blank">
+                            <a rel="noreferrer" href="https://www.instagram.com/william_park5/" target="_blank">
                                 <i className="fab fa-instagram"></i>
                             </a>
                         </li>
                         <li>
-                            <a rel="noreferrer" href="https://www.youtube.com/" target="_blank">
+                            <a rel="noreferrer" href="https://www.youtube.com/user/xGMxNaruto/featured" target="_blank">
                                 <i className="fab fa-youtube"></i>
                             </a>
                         </li>
                         <li>
-                            <a rel="noreferrer" href="https://web.whatsapp.com/" target="_blank">
+                            <a rel="noreferrer" href="https://wa.me/5215511925976" target="_blank">
                                 <i className="fab fa-whatsapp"></i>
                             </a>
                         </li>
